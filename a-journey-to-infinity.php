ab2903ba8f8f0415170e7307330fba6ab71d1f2e
cyberMart1 <123392303+cyberMart1@users.noreply.github.com>
2024-05-31 22:29:04 +0100

    Create a-journey-to-infinity.php

---
+++ a-journey-to-infinity.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<?php
+require_once "header.php";
+?>
+<!-- blog section start --> 
+<div class="about_section layout_padding">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-2 col-sm-12"></div>
+            <div class="col-lg-8 col-sm-12">
+                <div class="about_img"><img src="uncategorized_news/patiants.png"></div>
+                <h2 class="most_text">A JOURNEY TO INFINITY: Another Senseless Killing on the Streets of Lagos</h2>
+                <article>
+                    <p>
+                      <i>In the bustling streets of Ogba, Nigeria, an innocent life was tragically cut short by the hands of a police officer.</i>
+                    </p>
+                    <p>
+                      Innocent Uzoma, a 25-year-old mechanic, was returning home late one evening when he was 
+                      stopped at a police checkpoint. Without warning, a trigger-happy officer fired multiple shots at Innocent, killing him instantly.
+                    </p>
+                    <p>
+                      As news of the incident spread, shock and outrage reverberated through 
+                      the community. Innocent was known for his gentle nature and hard work. He had never been in trouble with the law.
+                        </p>
+                    <div class="google-auto-placed ap_container" style="width: 100%; height: auto; clear: both; text-align: center;"><ins data-ad-format="auto" class="adsbygoogle adsbygoogle-noablate" data-ad-client="ca-pub-7532470883667401" data-adsbygoogle-status="done" style="display: block; margin: auto; background-color: transparent; height: 280px;" data-ad-status="filled"><div id="aswift_2_host" style="border: none; height: 280px; width: 785px; margin: 0px; padding: 0px; position: relative; visibility: visible; background-color: transparent; display: inline-block; overflow: visible;"><iframe id="aswift_2" name="aswift_2" browsingtopics="true" style="left:0;position:absolute;top:0;border:0;width:785px;height:280px;" sandbox="allow-forms allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts allow-top-navigation-by-user-activation" width="785" height="280" frameborder="0" marginwidth="0" marginheight="0" vspace="0" hspace="0" allowtransparency="true" scrolling="no" allow="attribution-reporting; run-ad-auction" src="https://googleads.g.doubleclick.net/pagead/ads?gdpr=0&amp;us_privacy=1---&amp;gpp_sid=-1&amp;client=ca-pub-7532470883667401&amp;output=html&amp;h=280&amp;adk=726570301&amp;adf=297226417&amp;w=785&amp;abgtt=6&amp;fwrn=4&amp;fwrnh=100&amp;lmt=1715908181&amp;num_ads=1&amp;rafmt=1&amp;armr=3&amp;sem=mc&amp;pwprc=4770774342&amp;ad_type=text_image&amp;format=785x280&amp;url=https%3A%2F%2Fwww.vanguardngr.com%2F2013%2F09%2Fabuja-killings-truth-lies-politics%2F&amp;fwr=0&amp;pra=3&amp;rh=197&amp;rw=785&amp;rpe=1&amp;resp_fmts=3&amp;wgl=1&amp;fa=27&amp;uach=WyJXaW5kb3dzIiwiMTAuMC4wIiwieDg2IiwiIiwiMTI0LjAuNjM2Ny4xNTgiLG51bGwsMCxudWxsLCI2NCIsW1siQ2hyb21pdW0iLCIxMjQuMC42MzY3LjE1OCJdLFsiR29vZ2xlIENocm9tZSIsIjEyNC4wLjYzNjcuMTU4Il0sWyJOb3QtQS5CcmFuZCIsIjk5LjAuMC4wIl1dLDBd&amp;dt=1716015009821&amp;bpp=4&amp;bdt=9207&amp;idt=4&amp;shv=r20240515&amp;mjsv=m202405140101&amp;ptt=9&amp;saldr=aa&amp;abxe=1&amp;cookie=ID%3D68533713cfe706e0%3AT%3D1716015002%3ART%3D1716015002%3AS%3DALNI_MYW_3YaXSe8nDLmO2gt5dladKrzHA&amp;gpic=UID%3D00000d776abc7b82%3AT%3D1716015002%3ART%3D1716015002%3AS%3DALNI_MaisBV9Cj6hhDxltfpj-2B9m_fwbw&amp;eo_id_str=ID%3D3c5d8319939ff953%3AT%3D1716015002%3ART%3D1716015002%3AS%3DAA-Afja83i54uc4karyplb0Gzk-N&amp;prev_fmts=0x0%2C785x280&amp;nras=3&amp;correlator=1108410611412&amp;frm=20&amp;pv=1&amp;ga_vid=1894725176.1716015005&amp;ga_sid=1716015009&amp;ga_hid=860400416&amp;ga_fc=1&amp;u_tz=60&amp;u_his=1&amp;u_h=768&amp;u_w=1366&amp;u_ah=728&amp;u_aw=1366&amp;u_cd=24&amp;u_sd=1&amp;dmc=4&amp;adx=157&amp;ady=2757&amp;biw=991&amp;bih=640&amp;scr_x=0&amp;scr_y=199&amp;eid=44759875%2C44759926%2C44759837%2C31083587%2C31083823%2C42531705%2C44795922%2C95331982%2C95332924%2C95331712%2C95332415&amp;oid=2&amp;psts=AOrYGsmLhlW5Gir5dF18LUXIBlHnZI58rJiS31fvgvAS2qFEINGCO1aUCzZVeSVSc0zjW36kVkGnl5ShQXqYzAX9LYJFlZm8&amp;pvsid=1330876387245612&amp;tmod=304597336&amp;uas=0&amp;nvt=1&amp;fc=1408&amp;brdim=0%2C0%2C0%2C0%2C1366%2C0%2C1024%2C735%2C1008%2C640&amp;vis=1&amp;rsz=%7C%7Cs%7C&amp;abl=NS&amp;fu=128&amp;bc=31&amp;bz=1.02&amp;td=1&amp;psd=W251bGwsbnVsbCxudWxsLDNd&amp;nt=1&amp;ifi=4&amp;uci=a!4&amp;btvi=2&amp;fsb=1&amp;dtd=M" data-google-container-id="a!4" tabindex="0" title="Advertisement" aria-label="Advertisement" data-google-query-id="CNC9xYTPloYDFf4B-wMdzcwEmQ" data-load-complete="true"></iframe></div></ins></div>
+                    <p>
+                      The family Innocent was devastated. They demanded justice and accountability for the senseless killing of their son. However, their pleas seemed to fall on deaf ears.
+                    </p>
+                    <div id="attachment_415177" style="width: 422px" class="wp-caption alignnone"><a href="https://newlive.vanguardngr.com/?attachment_id=415177" rel="attachment wp-att-415177"><img decoding="async" aria-describedby="caption-attachment-415177" class="size-full wp-image-415177" alt="Survivors in hospital" src="https://newlive.vanguardngr.com/wp-content/uploads/2013/09/Abuja-Killings1.jpg" width="412" height="250" srcset="https://cdn.vanguardngr.com/wp-content/uploads/2013/09/Abuja-Killings1.jpg 412w, https://cdn.vanguardngr.com/wp-content/uploads/2013/09/Abuja-Killings1-300x182.jpg 300w" sizes="(max-width: 412px) 100vw, 412px"></a><p id="caption-attachment-415177" class="wp-caption-text">Survivors in hospital</p></div>
+                    <p>
+                        The police officer responsible for Innocent's death claimed self-defense, alleging that Innocent had been armed and had threatened him. However, witnesses at the scene vehemently denied this claim.
+                    </p>
+                    <p>
+                      As the investigation dragged on, it became clear that the truth was being suppressed. The police closed ranks, and evidence that could have incriminated the officer was conveniently lost.
+                    </p>
+                    <p>
+                        The community of Ogba was outraged. They organized protests and demanded answers. They refused to let the innocent death of one of their own be swept under the rug.
+                    </p>
+                    <p>
+                        Under mounting pressure, the police were forced to conduct an internal investigation. However, the findings were inconclusive, and no charges were ever filed against the officer.
+                    </p>
+                    <div class="ad-container remove-pseudo"> <div class="ad-container-inner"><div id="ad_placement_incontent_ad_2" data-display-index="3" "="" class="vanguard-admanager" data-processed="true"> </div> <amp-ad width="300" height="250" type="doubleclick" data-slot="/9223985/mobile-2" json="{'targeting':{'incontent-ad-id':'2'}}"> </amp-ad> </div></div>
+                    
+                    <p>
+                        Innocent's family and the community were left heartbroken and disillusioned. Justice had eluded them. The innocent life of a young man had been extinguished, and his killers remained unpunished.
+                    </p>
+                    <p>
+                      The tragic death of Innocent Uzoma became a grim reminder of the systemic failures and impunity that plagued the Nigerian police force. It was a story of injustice and shattered dreams, a tale that would forever haunt the streets of Ogba.
+                    </p>
+                    <div class="ad-container"> <div class="ad-container-inner"><div id="ad_placement_native" data-display-index="5" "="" class="vanguard-admanager" data-processed="true"> </div> <amp-ad width="300" height="250" type="doubleclick" data-slot="/9223985/VanguardNative" json="{'targeting':{'incontent-ad-id':'4'}}"> </amp-ad> </div></div>
+                    <p>
+                        =========
+                    </p>                    
+                </article>
+                
+                <div class="social_icon_main">
+                    <div class="social_icon">
+                        <ul>
+                            <li><a href="#"><img src="images/fb-icon.png"></a></li>
+                            <li><a href="#"><img src="images/twitter-icon.png"></a></li>
+                            <li><a href="#"><img src="images/instagram-icon.png"></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+        
+          
+                  <?php
+      require_once "footer.php";
+      ?>
+ 
+</body>
+</html>
