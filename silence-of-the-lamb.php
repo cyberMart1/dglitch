d77fac547f30a948ad49edbb220d020a6a2fc0c7
cyberMart1 <123392303+cyberMart1@users.noreply.github.com>
2024-06-01 07:57:29 +0100

    Create silence-of-the-lamb.php

---
+++ silence-of-the-lamb.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<?php
+require_once "header.php";
+?>
+<!-- blog section start --> 
+<div class="about_section layout_padding">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-2 col-sm-12"></div>
+            <div class="col-lg-8 col-sm-12">
+                <div class="about_img"><img src="uncategorized_news/sac_lamb.jpg"></div>
+                <h2 class="most_text">SILENCE OF THE LAMB: Another Sacrificial Lamb In The Hands Of Police Brutality </h2>
+                <article>
+                    <p>
+                      <i>The humid air of Lagos hung heavy, thick with the scent of rain and exhaust fumes. It was April 2013, and the streets of Ogba pulsed with the energy 
+                        of a city that never slept. But in a quiet corner of the bustling neighborhood, a tragedy unfolded, leaving a family shattered and a community reeling.</i>
+                    </p>
+                    <p>
+                      Chinedu had been returning home from the market on that fateful day, his arms laden with groceries. As he navigated the crowded streets, his eyes caught a 
+                      glimpse of a commotion. A group of young men were arguing, their voices rising in anger. Chinedu, ever the peacemaker, decided to intervene.
+                    </p>
+                    <p>
+                      He approached the group, his voice calm and measured, urging them to settle their differences peacefully. But his words were met with hostility. 
+                      One of the young men, his eyes filled with rage, lunged at Chinedu, brandishing a rusty knife
+                    </p>
+                    <div class="google-auto-placed ap_container" style="width: 100%; height: auto; clear: both; text-align: center;"><ins data-ad-format="auto" class="adsbygoogle adsbygoogle-noablate" data-ad-client="ca-pub-7532470883667401" data-adsbygoogle-status="done" style="display: block; margin: auto; background-color: transparent; height: 280px;" data-ad-status="filled"><div id="aswift_2_host" style="border: none; height: 280px; width: 785px; margin: 0px; padding: 0px; position: relative; visibility: visible; background-color: transparent; display: inline-block; overflow: visible;"><iframe id="aswift_2" name="aswift_2" browsingtopics="true" style="left:0;position:absolute;top:0;border:0;width:785px;height:280px;" sandbox="allow-forms allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts allow-top-navigation-by-user-activation" width="785" height="280" frameborder="0" marginwidth="0" marginheight="0" vspace="0" hspace="0" allowtransparency="true" scrolling="no" allow="attribution-reporting; run-ad-auction" src="https://googleads.g.doubleclick.net/pagead/ads?gdpr=0&amp;us_privacy=1---&amp;gpp_sid=-1&amp;client=ca-pub-7532470883667401&amp;output=html&amp;h=280&amp;adk=726570301&amp;adf=297226417&amp;w=785&amp;abgtt=6&amp;fwrn=4&amp;fwrnh=100&amp;lmt=1715908181&amp;num_ads=1&amp;rafmt=1&amp;armr=3&amp;sem=mc&amp;pwprc=4770774342&amp;ad_type=text_image&amp;format=785x280&amp;url=https%3A%2F%2Fwww.vanguardngr.com%2F2013%2F09%2Fabuja-killings-truth-lies-politics%2F&amp;fwr=0&amp;pra=3&amp;rh=197&amp;rw=785&amp;rpe=1&amp;resp_fmts=3&amp;wgl=1&amp;fa=27&amp;uach=WyJXaW5kb3dzIiwiMTAuMC4wIiwieDg2IiwiIiwiMTI0LjAuNjM2Ny4xNTgiLG51bGwsMCxudWxsLCI2NCIsW1siQ2hyb21pdW0iLCIxMjQuMC42MzY3LjE1OCJdLFsiR29vZ2xlIENocm9tZSIsIjEyNC4wLjYzNjcuMTU4Il0sWyJOb3QtQS5CcmFuZCIsIjk5LjAuMC4wIl1dLDBd&amp;dt=1716015009821&amp;bpp=4&amp;bdt=9207&amp;idt=4&amp;shv=r20240515&amp;mjsv=m202405140101&amp;ptt=9&amp;saldr=aa&amp;abxe=1&amp;cookie=ID%3D68533713cfe706e0%3AT%3D1716015002%3ART%3D1716015002%3AS%3DALNI_MYW_3YaXSe8nDLmO2gt5dladKrzHA&amp;gpic=UID%3D00000d776abc7b82%3AT%3D1716015002%3ART%3D1716015002%3AS%3DALNI_MaisBV9Cj6hhDxltfpj-2B9m_fwbw&amp;eo_id_str=ID%3D3c5d8319939ff953%3AT%3D1716015002%3ART%3D1716015002%3AS%3DAA-Afja83i54uc4karyplb0Gzk-N&amp;prev_fmts=0x0%2C785x280&amp;nras=3&amp;correlator=1108410611412&amp;frm=20&amp;pv=1&amp;ga_vid=1894725176.1716015005&amp;ga_sid=1716015009&amp;ga_hid=860400416&amp;ga_fc=1&amp;u_tz=60&amp;u_his=1&amp;u_h=768&amp;u_w=1366&amp;u_ah=728&amp;u_aw=1366&amp;u_cd=24&amp;u_sd=1&amp;dmc=4&amp;adx=157&amp;ady=2757&amp;biw=991&amp;bih=640&amp;scr_x=0&amp;scr_y=199&amp;eid=44759875%2C44759926%2C44759837%2C31083587%2C31083823%2C42531705%2C44795922%2C95331982%2C95332924%2C95331712%2C95332415&amp;oid=2&amp;psts=AOrYGsmLhlW5Gir5dF18LUXIBlHnZI58rJiS31fvgvAS2qFEINGCO1aUCzZVeSVSc0zjW36kVkGnl5ShQXqYzAX9LYJFlZm8&amp;pvsid=1330876387245612&amp;tmod=304597336&amp;uas=0&amp;nvt=1&amp;fc=1408&amp;brdim=0%2C0%2C0%2C0%2C1366%2C0%2C1024%2C735%2C1008%2C640&amp;vis=1&amp;rsz=%7C%7Cs%7C&amp;abl=NS&amp;fu=128&amp;bc=31&amp;bz=1.02&amp;td=1&amp;psd=W251bGwsbnVsbCxudWxsLDNd&amp;nt=1&amp;ifi=4&amp;uci=a!4&amp;btvi=2&amp;fsb=1&amp;dtd=M" data-google-container-id="a!4" tabindex="0" title="Advertisement" aria-label="Advertisement" data-google-query-id="CNC9xYTPloYDFf4B-wMdzcwEmQ" data-load-complete="true"></iframe></div></ins></div>
+                    <p>
+                      Chinedu caught off guard, stumbled back, his heart pounding in his chest. He raised his hands in a gesture of surrender, pleading for calm. 
+                      But his pleas were drowned out by the escalating tension.
+                    </p>
+                    <div id="attachment_415177" style="width: 422px" class="wp-caption alignnone"><a href="https://newlive.vanguardngr.com/?attachment_id=415177" rel="attachment wp-att-415177"><img decoding="async" aria-describedby="caption-attachment-415177" class="size-full wp-image-415177" alt="Survivors in hospital" src="https://newlive.vanguardngr.com/wp-content/uploads/2013/09/Abuja-Killings1.jpg" width="412" height="250" srcset="https://cdn.vanguardngr.com/wp-content/uploads/2013/09/Abuja-Killings1.jpg 412w, https://cdn.vanguardngr.com/wp-content/uploads/2013/09/Abuja-Killings1-300x182.jpg 300w" sizes="(max-width: 412px) 100vw, 412px"></a><p id="caption-attachment-415177" class="wp-caption-text">Survivors in hospital</p></div>
+                    <p>
+                      Suddenly, a police patrol car screeched to a halt nearby. Two officers, their faces etched with the weariness of their job, rushed towards the scene. 
+                      The young man with the knife, seeing the officers, fled into the maze of alleyways.
+                    </p>
+                    <p>
+                      But the officers, their eyes fixed on Chinedu, saw only a threat. One of them, a burly man with a face hardened by years of policing a city on the edge, 
+                      drew his gun. A single shot rang out, shattering the afternoon stillness.
+                    </p>
+                    <p>
+                      Chinedu crumpled to the ground, a crimson stain blossoming on his white shirt. Ada, who had been following her husband from a distance, screamed, 
+                      her voice a raw, piercing cry that echoed through the narrow streets.
+                    </p>
+                    <p>
+                       The officers, their faces blank, stood over Chinedu's body, their guns still drawn. They had seen a suspect, a potential threat, and they had reacted. But in their haste, they had failed to see the man behind the fear, 
+                      the father, the husband, the innocent soul caught in the crossfire of a city struggling with its demons.
+                    </p>
+                    <div class="ad-container remove-pseudo"> <div class="ad-container-inner"><div id="ad_placement_incontent_ad_2" data-display-index="3" "="" class="vanguard-admanager" data-processed="true"> </div> <amp-ad width="300" height="250" type="doubleclick" data-slot="/9223985/mobile-2" json="{'targeting':{'incontent-ad-id':'2'}}"> </amp-ad> </div></div>
+                    
+                    <p>
+                      News of Chinedu's death spread like wildfire through Ogba. The community, united in grief and anger, demanded justice. But the justice they 
+                      sought was elusive. The officer who fired the fatal shot was never charged, his actions were deemed justified.
+                    </p>
+                    <p>
+                      The memory of Chinedu, the good man who was killed by a stray bullet, continued to haunt the streets of Ogba. His story became a grim reminder 
+                      of the fragility of life, the brutality of injustice, and the enduring power of an innocent life cut short.
+                    </p>
+                    <div class="ad-container"> <div class="ad-container-inner"><div id="ad_placement_native" data-display-index="5" "="" class="vanguard-admanager" data-processed="true"> </div> <amp-ad width="300" height="250" type="doubleclick" data-slot="/9223985/VanguardNative" json="{'targeting':{'incontent-ad-id':'4'}}"> </amp-ad> </div></div>
+                    <p>
+                      ===========================<br>
+                      Written by Victoria Odufuwa<br>
+                      ===========================
+                    </p>                    
+                </article>
+                
+                <div class="social_icon_main">
+                    <div class="social_icon">
+                        <ul>
+                            <li><a href="#"><img src="images/fb-icon.png"></a></li>
+                            <li><a href="#"><img src="images/twitter-icon.png"></a></li>
+                            <li><a href="#"><img src="images/instagram-icon.png"></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+        
+          
+                  <?php
+      require_once "footer.php";
+      ?>
+ 
+</body>
+</html>
